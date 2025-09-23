95515fe82da6534d5f82d53bd95eabe3a4620d4a
tankor-cpu <tanakornienko08@gmail.com>
2025-09-23 15:08:50 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//Include GLEW
+#include <GL/glew.h>
+//Include GLFW
+#include <GLFW/glfw3.h>
+
+float points[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+   -0.5f, -0.5f, 0.0f
+};
+
+const char* vertex_shader =
+"#version 330\n"
+"layout(location=0) in vec3 vp;"
+"void main () {"
+"     gl_Position = vec4 (vp, 1.0);"
+"}";
+
+
+
+const char* fragment_shader =
+"#version 330\n"
+"out vec4 fragColor;"
+"void main () {"
+"     fragColor = vec4 (0.5, 0.0, 0.5, 1.0);"
+"}";
+
